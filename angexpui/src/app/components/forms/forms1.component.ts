12741aaded3740325ea4dc225183bb145d138181
Michael Voronov <michael.voronov@gmail.com>
2019-10-13 10:56:05 +0300

    input style

--- angexpui/src/app/components/forms/forms1.component.ts
+++ angexpui/src/app/components/forms/forms1.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-forms1',
   templateUrl: './forms1.component.html',
-  styleUrls: ['./forms1.component.scss']
+  styleUrls: ['./forms1.component.scss', 'form1-ut.component.scss']
 })
 export class Forms1Component implements OnInit {
 
