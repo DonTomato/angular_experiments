49858eca7f8db12d11c0c1d848318a907f8115e6
Michael Voronov <michael.voronov@gmail.com>
2019-10-13 12:26:59 +0300

    input styles

--- angexpui/src/app/app.module.ts
+++ angexpui/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
+import { FlexLayoutModule } from '@angular/flex-layout'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HookComponent, NestedHookComponent, Forms1Component } from './components';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -21,6 +22,7 @@ import { environment } from '../environments/environment';
     BrowserModule,
     AppRoutingModule,
     ClarityModule,
+    FlexLayoutModule,
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
